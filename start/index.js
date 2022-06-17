7f73feb9dd23a656b457640d760531b6aa998bc4
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2022-06-17 16:06:48 -0700

    Create file index.js

---
+++ start/index.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            productName: 'Book a Cruise to Moon',
+            productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.'
+        }
+    }
+});
