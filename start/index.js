73f860079be28fbbc57cc24fc8970e2ae585e67f
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2022-06-17 19:13:36 -0700

    Add productImage to index.js

--- start/index.js
+++ start/index.js
@@ -2,7 +2,13 @@ const app = Vue.createApp({
     data(){
         return{
             productName: 'Book a Cruise to Moon',
-            productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.'
+            productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+            // additional properties later
+            productImage: 'assets/cruise.jpg',
+            productImageDescription: 'An astronaut floats outside the window while you sit in comfort',
+            productImageStyle: {
+                'border-radius': '15px'
+            }
         }
     }
 });
